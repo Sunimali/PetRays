1751bed65ee402a9dae076affc79b5270a5183e5
Sunimali Rathnayake <sunimalirathnayake@gmail.com>
2019-01-09 20:30:15 +0530

    add calander picker and time picker

--- app/src/main/java/com/example/sunimali/petrays/AppointmentActivity.java
+++ app/src/main/java/com/example/sunimali/petrays/AppointmentActivity.java
@@ -1,12 +1,16 @@
 package com.example.sunimali.petrays;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -21,6 +25,12 @@ public class AppointmentActivity extends AppCompatActivity {
     TextView location,time,date;
     int PLACE_PICKER_REQUEST = 1;
     String locationadd;
+    CalendarView calendarView;
+    String datee;
+    int minute;
+    int hour;
+    String timee;
+    static  int DIALOG_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +38,13 @@ public class AppointmentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_appointment);
 
         location = (TextView)findViewById(R.id.textViewLocation);
-        time = (TextView)findViewById(R.id.textViewTime);
+        time = (TextView)findViewById(R.id.textViewtime);
         locationb = ( Button)findViewById(R.id.buttonloca);
         timeb = ( Button)findViewById(R.id.buttonTime);
+        send = ( Button)findViewById(R.id.buttonsend);
+        calendarView = (CalendarView)findViewById(R.id.calendarView);
+
+
 
         locationb.setOnClickListener(
                 new View.OnClickListener() {
@@ -42,11 +56,8 @@ public class AppointmentActivity extends AppCompatActivity {
                         try {
 
                             PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-
                             startActivityForResult(builder.build(AppointmentActivity.this), PLACE_PICKER_REQUEST);
 
-                            //intent = builder.build(AppointmentActivity.this);
-                           // startActivityForResult(PLACE_PICKER_REQUEST,RESULT_OK,intent);
                         } catch (GooglePlayServicesRepairableException e) {
                             e.printStackTrace();
                         } catch (GooglePlayServicesNotAvailableException e) {
@@ -56,6 +67,48 @@ public class AppointmentActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        calendarView.setOnDateChangeListener(
+                new CalendarView.OnDateChangeListener() {
+                    @Override
+                    public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+
+                        datee = year+"-"+month+"-"+dayOfMonth;
+
+                    }
+                }
+        );
+
+        send.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+      //  showTimePickerDialog();
+        timeb.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(AppointmentActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                                timee = hourOfDay + ":" + minutes;
+                                time.setText(hourOfDay + ":" + minutes);
+                                showtime();
+
+                            }
+                        }, 0, 0, false);
+                        timePickerDialog.show();
+
+                    }
+                }
+        );
+    }
+    public void showtime(){
+        Toast.makeText(this,timee,Toast.LENGTH_LONG).show();
     }
 
     public void onActivityResult(int requestCode, int resultCode ,Intent data) {
@@ -67,4 +120,37 @@ public class AppointmentActivity extends AppCompatActivity {
 
         }
     }
+
+  /*  public void showTimePickerDialog(){
+        timeb.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDialog(DIALOG_ID);
+                    }
+                }
+        );
+    }*/
+
+    //@Override
+   /* protected Dialog onCreateDialog(int id) {
+        if(id==DIALOG_ID){
+           return new TimePickerDialog(AppointmentActivity.this,kTimepickerListner,hour,minute,false);
+
+        }
+        return  null;
+    }*/
+
+  /*  protected TimePickerDialog.OnTimeSetListener kTimepickerListner=
+            new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minutee) {
+                    hour = hourOfDay;
+                    minute = minutee;
+                    timee = hour+":"+minute;
+                    time.setText(timee);
+
+                }
+            };*/
+
 }
